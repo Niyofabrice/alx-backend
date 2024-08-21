92025a5773e3e68d0376b29f8709e189f072601f
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-21 11:57:41 -0700

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Basic dictionary
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCaching(BaseCaching):
+    """
+    Class that inherits from BaseCaching and is a caching system
+    """
+    def put(self, key, item):
+        """
+        Function that assings items to the dictionary
+        """
+        if key or item is not None:
+            self.cache_data[key] = item
+    def get(self, key):
+        """
+        Function that return the value in self.cache_data based on key
+        """
+        return self.cache_data.get(key)
